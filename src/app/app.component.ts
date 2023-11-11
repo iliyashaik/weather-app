9aa75f7f13a752f94a3d0252e079f08add852b71
إلياس شيخ <shaiks@Iliyazs-MacBook-Air.local>
2023-11-11 19:43:01 +0100

    app created

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {api} from './services/api.service'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'weather-desktop';
+
+  constructor(private service: api) { }
+
+  zipcode: string = '';
+  temparature: Number = 0;
+  country: string = '';
+  cityName: string = '';
+  mintemparature: Number = 0;
+  maxtemparature: Number = 0;
+  humidity: Number = 0;
+  feelslike: string = '';
+
+  getData(){
+    this.service.fetchData(this.zipcode).subscribe((response: any) => {
+      console.log(response, 'response')
+      this.temparature = response.main.temp;
+      this.country = response.sys.country;
+      this.cityName = response.name;
+      this.maxtemparature = response.main.temp_max;
+      this.mintemparature = response.main.temp_min;
+      this.humidity = response.main.humidity
+      this.feelslike= response.weather[0].description;
+    });
+
+  }
+}
