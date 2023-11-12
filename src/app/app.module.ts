3eac0f09778b4509b27038db87850f47d3eb90dd
Iliyaz Shaik <iliyas.shaik3@gmail.com>
2023-11-12 21:06:58 +0100

    removed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {APP_BASE_HREF} from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,7 +20,7 @@ import { api } from './services/api.service';
     HttpClientModule,
     BrowserAnimationsModule
   ],
-  providers: [api],
+  providers: [api, {provide: APP_BASE_HREF, useValue: '/home'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
